e9cd7f487b2c7a82cb67dab1e04084ca8d4e7d3b
Evan Butler <evanb2@mac.com>
2015-04-10 11:36:17 -0700

    pass second spec

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ var makeTriangle = function(side1, side2, side3) {
 if (side1 + side2 < side3 || side2 + side3 < side1 || side1 + side3 < side2)
 {
     return "False";
+} else (side1 === side2 === side3)
+{
+    return "Equilateral";
 }
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe("makeTriangle", function() {
     it("returns false if the user inputs a non-triangle", function() {
         expect(makeTriangle(2, 2, 8)).to.equal("False");
     });
+
+    it("returns 'equilateral' if the user inputs 3 equal numbers", function() {
+        expect(makeTriangle(8, 8, 8)).to.equal("Equilateral");
+    });
 });
