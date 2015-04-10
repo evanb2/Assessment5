80c9d3a1f932468930b482389546695afb64bb55
Evan Butler <evanb2@mac.com>
2015-04-10 13:54:14 -0700

    add parseint to jquery

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,14 @@ var makeTriangle = function(side1, side2, side3) {
 
     var output = "Your Triangle is ";
 
-    if (side1 + side2 < side3 || side2 + side3 < side1 || side1 + side3 < side2) {
-        output = "";
-        alert("This is not a triangle!");
+    if (side1 + side2 < side3 || side2 + side3 < side1 || side1 + side3 < side2
+        || side1 === 0 ||side2 === 0 || side3 ===0) {
+        output = "This is not a triangle!";
     } else if (side1 === side2 && side1 === side3) {
         output += "Equilateral.";
     } else if (side1 === side2 || side2 === side3 || side1 === side3) {
         output += "Isosceles.";
-    } else if (side1 !== side2 && side1 !== side3 && side2 !== side3) {
+    } else {
         output += "Scalene.";
     }
 
@@ -18,10 +18,11 @@ var makeTriangle = function(side1, side2, side3) {
 };
 
 $(document).ready(function() {
+    $("#side1").focus();
     $("form#triangle").submit(function(event) {
-        var side1 = $("input#side1").val();
-        var side2 = $("input#side2").val();
-        var side3 = $("input#side3").val();
+        var side1 = parseInt($("input#side1").val());
+        var side2 = parseInt($("input#side2").val());
+        var side3 = parseInt($("input#side3").val());
         var result = makeTriangle(side1, side2, side3);
 
         $(".newtriangle").text(result);
--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,21 @@
 describe("makeTriangle", function() {
     it("returns false if the user inputs a non-triangle", function() {
-        expect(makeTriangle(2, 2, 8)).to.equal("This is not a triangle");
+        expect(makeTriangle(2, 2, 8)).to.equal("This is not a triangle!");
     });
 
     it("returns 'Equilateral' if the user inputs 3 equal numbers", function() {
-        expect(makeTriangle(8, 8, 8)).to.equal("Equilateral");
+        expect(makeTriangle(8, 8, 8)).to.equal("Your Triangle is Equilateral.");
     });
 
     it("returns 'Isosceles' if the user inputs any 2 equal sides", function() {
-        expect(makeTriangle(3, 3, 2)).to.equal("Isosceles");
+        expect(makeTriangle(3, 3, 2)).to.equal("Your Triangle is Isosceles.");
     });
 
     it("returns 'Scalene' if the user inputs 3 unique sides", function() {
-        expect(makeTriangle(3, 4, 2)).to.equal("Scalene");
+        expect(makeTriangle(3, 4, 2)).to.equal("Your Triangle is Scalene.");
+    });
+
+    it("returns 'Isosceles' if the user inputs any 2 equal sides", function() {
+        expect(makeTriangle(3, 3, 4)).to.equal("Your Triangle is Isosceles.");
     });
 });
