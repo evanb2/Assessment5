d9b1e44338a8a9378a831a627e97c033610c95ec
Evan Butler <evanb2@mac.com>
2015-04-10 12:11:57 -0700

    pass third spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,22 @@
 var makeTriangle = function(side1, side2, side3) {
 
-if (side1 + side2 < side3 || side2 + side3 < side1 || side1 + side3 < side2)
-{
-    return "False";
-} else (side1 === side2 === side3)
-{
-    return "Equilateral";
-}
+    var output = "";
+
+
+    if (side1 + side2 < side3 || side2 + side3 < side1 || side1 + side3 < side2) {
+        output = "This is not a triangle";
+    } else if (side1 === side2 && side1 === side3) {
+        output = "Equilateral";
+    } else if (side1 === side2 || side2 === side3 || side1 === side3) {
+        output = "Isosceles";
+    } else if (side1 !== side2 && side1 !== side3 && side2 !== side3) {
+        output = "Scalene";
+    }
 
 
 
 
 
-
+    return output;
 
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,17 @@
 describe("makeTriangle", function() {
     it("returns false if the user inputs a non-triangle", function() {
-        expect(makeTriangle(2, 2, 8)).to.equal("False");
+        expect(makeTriangle(2, 2, 8)).to.equal("This is not a triangle");
     });
 
-    it("returns 'equilateral' if the user inputs 3 equal numbers", function() {
+    it("returns 'Equilateral' if the user inputs 3 equal numbers", function() {
         expect(makeTriangle(8, 8, 8)).to.equal("Equilateral");
     });
+
+    it("returns 'Isosceles' if the user inputs any 2 equal sides", function() {
+        expect(makeTriangle(3, 3, 2)).to.equal("Isosceles");
+    });
+
+    // it("returns 'Scalene' if the user inputs any 2 equal sides", function() {
+    //     expect(makeTriangle(3, 3, 2)).to.equal("Isosceles");
+    // });
 });
