c7af6015d1a60284a4c0d59c6dbf9f25177bfc5e
Evan Butler <evanb2@mac.com>
2015-04-10 12:30:02 -0700

    add jquery to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,17 @@ var makeTriangle = function(side1, side2, side3) {
     return output;
 
 };
+
+$(document).ready(function() {
+    $("form#triangle").submit(function(event) {
+        var side1 = $("input#side1").val();
+        var side2 = $("input#side2").val();
+        var side3 = $("input#side3").val();
+        var result = makeTriangle(side1, side2, side3);
+
+        $(".newtriangle").text(result);
+
+        $("#result").show();
+        event.preventDefault();
+    });
+});
